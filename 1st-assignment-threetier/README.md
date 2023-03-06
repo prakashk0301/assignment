38de80610a79ddc45d01c19ebfffcc8d7b8e4d3f
prakash <prakash@gmail.com>
2023-03-06 16:59:42 +0530

    assignment

---
+++ 1st-assignment-threetier/README.md
@@ -0,0 +1,1 @@
+In order to create AWS ELB, ec2, RDS you need to update variables, Security groups, Subnets, Key, AMI ID, Region, AZ , VPC etc
---
+++ 1st-assignment-threetier/var.tf
@@ -0,0 +1,34 @@
+variable "aws_vpc" {
+    description = "VPC"
+    default = "vpc-0e8467f076fa127e9"
+}
+
+variable "aws_subnet" {
+    description = "aws subnet1a"
+    default = "subnet-0325154992ec9b4c5"
+}
+
+variable "aws_subnet2" {
+    description = "aws subnet1b"
+    default = "subnet-0fc8213f59cffd5fd"
+}
+
+variable "aws_key" {
+    description = "aws key"
+    default = "aws-frankfurt"
+}
+
+ variable "aws_ami" {
+     description = "ami id"
+     default = "ami-09439f09c55136ecf"
+}
+
+ variable "instance_type" {
+     description = "instance_type"
+     default = "t2.micro"
+}
+
+ variable "instance_class" {
+     description = "instance_class"
+     default = "db.t2.micro"
+}
